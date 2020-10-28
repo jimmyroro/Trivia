2714d685a040199c092da9d41c56bd1f4ed9a499
jimmyroro <jimmyroro58@gmail.com>
2020-10-28 16:45:25 -0500

    conditionally render login and high score

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
-import React, { useState, useEffect, FC } from 'react'
+import './App.css';
+import React, { useState, useEffect, FC } from 'react';
+import Question from './Question';
 
 function App () {
   const [storage, setStorage] = useState([]);
   const [user, setUser] = useState('');
   const [highScore, setHighScore] = useState('Zero');
+  const [loggedIn, setLoggedIn] = useState(false)
 
   const checkUser = () => {
     // if user exists, set their high score in state
@@ -12,6 +14,7 @@ function App () {
       // TODO: Remove console logs
       console.log('user exists');
       setHighScore(localStorage.getItem(user))
+      
     }
     // if they don't exist, save their username
     else {
@@ -19,25 +22,34 @@ function App () {
       console.log('no exist')
       localStorage.setItem(user, 'Zero')
     }
+    setLoggedIn(true);
   }
 
   return (
     <div>
       <h5>Please enter a name!</h5>
-      <h3>current high score: {highScore}</h3>
-      <form>
-        <div class='form-group'>
-          <input
-            onChange={(e) => setUser(e.target.value)}
-            type='text'
-            class='form-control'
-            aria-describedby='username'
-          ></input>
+      {loggedIn && 
+        <div>
+          <h3>current high score: {highScore}</h3>
+          <h3>Logged in as {user}</h3>
+          <Question/>
         </div>
-        <button type='button' class='btn btn-primary' onClick={() => checkUser()}>
-          Submit
-        </button>
-      </form>
+      }
+      {!loggedIn && 
+        <form>
+          <div class='form-group'>
+            <input
+              onChange={(e) => setUser(e.target.value)}
+              type='text'
+              class='form-control'
+              aria-describedby='username'
+            ></input>
+          </div>
+          <button type='button' class='btn btn-primary' onClick={() => checkUser()}>
+            Submit
+          </button>
+        </form>
+      }
     </div>
   )
 }
