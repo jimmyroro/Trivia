76e7d305fe0120552ffcf3fa14c3067add2eb881
jimmyroro <jimmyroro58@gmail.com>
2020-10-29 18:50:55 -0500

    pass scores from App to Question

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,45 @@ import Question from './Question';
 function App () {
   const [storage, setStorage] = useState([]);
   const [user, setUser] = useState('');
-  const [highScore, setHighScore] = useState(0);
+  const [highScore, setHighScore] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false)
+  const [currentScore, setCurrentScore] = useState(0);
 
   const checkUser = () => {
-    // if user exists, set their high score in state
+    // if user exists, retrieve info from localstorage and set their high score in state as a number
     if (localStorage.getItem(user)) {
       // TODO: Remove console logs
       console.log('user exists');
-      setHighScore(localStorage.getItem(user))
-      
+      setHighScore(parseInt(localStorage.getItem(user)))
     }
     // if they don't exist, save their username
     else {
       // TODO: Remove console logs
       console.log('no exist')
       localStorage.setItem(user, 0)
+      setHighScore(0)
     }
     setLoggedIn(true);
   }
 
   return (
     <div>
-      <h5>Please enter a name!</h5>
       {loggedIn && 
         <div>
           <h3>current high score: {highScore}</h3>
+          <h3>current score: {currentScore}</h3>
           <h3>Logged in as {user}</h3>
-          <Question/>
+          <Question
+            highScore={highScore}
+            setHighScore={setHighScore}
+            currentScore={currentScore}
+            setCurrentScore={setCurrentScore}
+          />
         </div>
       }
       {!loggedIn && 
+      <div>
+        <h5>Please enter a name!</h5>
         <form>
           <div class='form-group'>
             <input
@@ -49,6 +57,7 @@ function App () {
             Submit
           </button>
         </form>
+      </div>
       }
     </div>
   )
