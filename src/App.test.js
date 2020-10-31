41308b44666fc358bb9b94257bd3db1dd8f3332e
jimmyroro <jimmyroro58@gmail.com>
2020-10-31 13:55:37 -0500

    test that App renders

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+import ReactDOM from 'react-dom';
 import App from './App';
+import Question from './Question';
+import Header from './Header';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+configure({ adapter: new Adapter() })
+
+describe('App', () => {
+  test('renders App component', () => {
+    shallow(<App/>)
+  })
+
+
+    // test('questions should not duplicate', () => {
+
+    // })
+
+    // test('after clicking submit, the answer should display', () => {
+// 
+    // })
+})
+
