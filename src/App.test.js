edb617af725b45c7c80319547c7c97cdb567f191
jimmyroro <jimmyroro58@gmail.com>
2020-10-31 15:20:56 -0500

    test that clicking next question displays a new question

--- src/App.test.js
+++ src/App.test.js
@@ -11,30 +11,43 @@ import Header from './Header';
 configure({ adapter: new Adapter() })
 
 describe('App', () => {
-  test('renders App component', () => {
+  xtest('renders App component', () => {
     shallow(<App/>)
   })
 
-  test('can enter a username', () => {
+  xtest('can enter a username', () => {
     render(<App/>)
     expect(screen.getByRole('textbox')).toBeInTheDocument();
     userEvent.type(screen.getByRole('textbox'), 'dummyUser{enter}');
     expect(screen.getByText(/dummyUser/)).toBeInTheDocument();
   })
 
-  test('the navbar should display a currentScore and highScore upon login', () => {
+  xtest('the navbar should display a currentScore and highScore upon login', () => {
     render(<App/>)
     userEvent.type(screen.getByRole('textbox'), 'dummyUser{enter}');
     expect(screen.getByText(/High score: 0/)).toBeInTheDocument();
     expect(screen.getByText(/Current score: 0/)).toBeInTheDocument();
   })
 
+  xtest('after clicking submit, the answer should display', () => {
+    render(<Question />)
+    userEvent.click(screen.getByTestId("answer0"));
+    userEvent.click(screen.getByRole('button'));
+    expect(screen.getByText(/The correct answer is: /)).toBeInTheDocument();
+  })
+
+  test('clicking next question button should show another question', async () => {
+    render(<Question/>)
+    const firstQuestion = screen.getByRole('heading').innerHTML;
+    await userEvent.click(screen.getByTestId("answer0"));
+    await userEvent.click(screen.getByRole('button'));
+    await userEvent.click(screen.getByRole('button'));
+    const secondQuestion = screen.getByRole('heading').innerHTML;
+    await expect(firstQuestion).not.toEqual(secondQuestion);
+  })
     // test('questions should not duplicate', () => {
 
     // })
 
-    // test('after clicking submit, the answer should display', () => {
-// 
-    // })
 })
 
